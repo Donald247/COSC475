8811bf7a64588bc373c6c0e2d4c9952641928c26
alexmchughnz <34287127+alexmchughnz@users.noreply.github.com>
2018-10-12 13:20:54 +1300

    Can place handle in grinder.

--- robotics.m
+++ robotics.m
@@ -3,9 +3,26 @@
 
 clear, clc, close all
 
-T0 = SO3;
+p_grinder = [457.10, -526.49, 580.00];
+R_grinder = rotz(105, 'deg');
+T_grinder = rt2tr(R_grinder, p_grinder);
 
 
-mdl_ur5;
+p_PF2_grinder = [280.21, 0, -516.45];
+R_PF2_grinder = roty(-90, 'deg');
+T_PF2_grinder = rt2tr(R_PF2_grinder, p_PF2_grinder);
 
-ur5.teach();
+
+p_tool_PF2 = [32.0; 0; -27.56];
+T_tool_PF2 = transl(p_tool_PF2)
+
+
+% r_PF2_tool = SO3.rpy([0, 90, 0]); %deg
+% T_tool_PF2 = transl(-p_PF2_tool) * rpy2tr(-r_PF2_tool, 'deg');
+% 
+% T_tool = T_PF2 * T_tool_PF2;
+
+
+
+
+
